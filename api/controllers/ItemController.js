24acaca98d8e7cffc1e06323609abf3463db9829
raymonddavis <raymond.davis.4th@gmail.com>
2016-11-01 16:12:09 -0500

    Working State, Needs Upated Error Messages

--- api/controllers/ItemController.js
+++ api/controllers/ItemController.js
@@ -6,12 +6,53 @@
  */
 
 module.exports = {
-	search: function (req, res, next) {
-		var departmentId = req.param('did');
-		var buildingId = req.param('bid');
-		var roomId = req.param('rid');
+    search: function(req, res, next) {
+        var departmentId = req.param('did');
+        var buildingId = req.param('bid');
+        var roomId = req.param('rid');
 
-		return res.ok(departmentId + " - " + buildingId + " - " + roomId);
-	}
+        var _items = [];
+
+        var params = {};
+
+        if (departmentId) params = {id: departmentId};
+
+        Department.find().where(params).populate('buildings').exec(function(err, departments) {
+            if (err || !departments || departments.length == 0) return next(sails.config.additionals.DEPARTMENT_NOT_FOUND);
+
+            var buildings = [];
+            if (!buildingId) {
+	            for (var x in departments) {
+	                var building = departments[x].buildings;
+	                for (var y in building) {
+	                    if (building[y].id != null) buildings.push(building[y].id);
+	                }
+	            }
+	          } else {
+	          	buildings.push(buildingId);
+	          }
+
+	          params = {building: buildings};
+	   
+            Room.find().where(params).exec(function(err, rooms) {
+                if (err || !rooms || rooms.length == 0) return next(sails.config.additionals.ROOM_NOT_FOUND);
+
+                var room = [];
+                if (!roomId) {
+	                for (var x in rooms) {
+	                    if (rooms[x].id != null) room.push(rooms[x].id);
+	                }
+	              } else {
+	              	room.push(roomId);
+	              }
+
+	              params = {room: room};
+
+                Item.find().where(params).exec(function(err, items) {
+                    if (err || !items || items.length == 0) return next(sails.config.additionals.ITEM_NOT_FOUND);
+                    return res.ok(items);
+                });
+            });
+        });
+    }
 };
-
